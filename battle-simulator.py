d6da86c8e03b609d5ecc9213ce3f54aefc159b36
kirill <035386@iskra.local>
2021-06-15 15:14:38 +0300

    new implementation

--- battle-simulator.py
+++ battle-simulator.py
@@ -2,45 +2,71 @@ from random import randrange
 
 
 class Unit:
-    """Each unit represents either a soldier or a vehicle maned by a predetermined number of soldiers.
+    """Each unit represents either a soldier or a vehicle maned by a predetermined number of soldiers
+
     """
-    def __init__(self):
-        self.health = 100  # Represents the health of the unit
-        self.recharge = range(100, 2000)  # Represents the number of ms required to recharge the unit for an attack
+
+    def __init__(self, health, recharge):
+        self.health = health  # Represents the health of the unit
+        self.recharge = recharge  # Represents the number of ms required to recharge the unit for an attack
+
+    def is_alive(self):
+        """ Check if Unit is alive """
+        if self.health > 0:
+            return True
+        else:
+            return False
 
 
 class Soldier(Unit):
     """Soldiers are units that have an additional property
-    """
-    def __init__(self):
-        super().__init__()
-        self.experience = 0  # Represents the soldier experience
 
-    def attack(self):
-        """Soldiers attack success probability"""
+    """
+
+    def __init__(self):
+        self.experience = 0  # Represents the soldier experience
+        Unit.__init__(self, 100, randrange(100, 2000))
+
+    def success_attack(self):
+        """ Soldiers attack success probability """
         return 0.5 * (1 + self.health/100) * randrange(50 + self.experience, 100) / 100
 
-    def damage(self):
-        """The amount of damage a soldier can afflict"""
+    def damage_amount(self):
+        """ The amount of damage a soldier can afflict """
         return 0.05 + self.experience / 100
+
+    def do_attack(self, enemy):
+        pass
+
+    def get_damage(self, damage):
+        self.health -= damage
+        if self.health < 0:
+            self.health = 0
 
 
 class Vehicle(Unit):
     """A battle vehicle
-    """
-    def __init__(self):
-        super().__init__()
-        self.operators = None  # The number of soldiers required to operate the vehicle
-        self.recharge = range(1000, 2000)  # The recharge property for a vehicle must be greater than 1000 (ms)
 
-    def attack(self):
+    """
+
+    def __init__(self):
+        self.operators = randrange(1, 3)  # The number of soldiers required to operate the vehicle
+        Unit.__init__(self, 100, randrange(1000, 2000))
+
+    def success_attack(self):
         pass
 
-    def damage(self):
+    def damage_amount(self):
         pass
 
 
 class Squad:
     """Squads are consisted out of a number of units (soldiers or vehicles), that behave as a coherent group
+
     """
+
+    pass
+
+
+class Army:
     pass
